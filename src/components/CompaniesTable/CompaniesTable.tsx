0524b8eab86c993b112a27d1ec34306d0006275a
William Fernandes Dias <william.winchester1967@gmail.com>
2023-11-28 18:56:42 -0300

    fix: pass page count to data table

--- src/components/CompaniesTable/CompaniesTable.tsx
+++ src/components/CompaniesTable/CompaniesTable.tsx
@@ -78,6 +78,7 @@ export default function CompaniesTable({
             }}
             autoHeight
             getRowId={row => row.cnpj}
+            rowCount={count}
           />
         </Box>
       </Paper>
