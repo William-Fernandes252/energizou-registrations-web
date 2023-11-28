e659be073626d6943d1112cc61b124d432185dd2
William Fernandes Dias <william.winchester1967@gmail.com>
2023-11-27 21:49:08 -0300

    fix: set loader on company list page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,32 +5,34 @@ import type { RouteObject } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
 import RegistrationPage from '@/pages/RegistrationPage/RegistrationPage';
 import type { AxiosInstance } from 'axios';
+import { redirectToLoginOnUnauthorized } from './utils';
 
 export function createRoutes(axios: AxiosInstance): RouteObject[] {
   return [
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <IndexPage />,
-      },
-      {
-        path: 'companies',
-        element: <CompanyListPage />,
-        children: [
-          {
-            path: 'new',
-            element: <RegistrationPage />,
-          },
-        ],
-      },
-      {
-        path: 'login',
-        element: <LoginPage />,
-      },
-    ],
-  },
-];
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <IndexPage />,
+        },
+        {
+          path: 'companies',
+          loader: redirectToLoginOnUnauthorized(getCompaniesLoader(axios)),
+          element: <CompanyListPage />,
+          children: [
+            {
+              path: 'new',
+              element: <RegistrationPage />,
+            },
+          ],
+        },
+        {
+          path: 'login',
+          element: <LoginPage />,
+        },
+      ],
+    },
+  ];
 }
